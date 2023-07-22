6beafbedf6e29c6d2f4c2d2ee685cf1006f6b9e4
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-07-22 11:46:25 +0200

    marvin now can insult your coding style, ctrl z works with nc

--- includes/Bot.hpp
+++ includes/Bot.hpp
@@ -11,6 +11,7 @@ class Marvin {
 	~Marvin();
 	void setBotJokes();
 	void constructBot();
+	void setBotInsults();
 	void setBotAiModelExcuse();
 	std::string getBotName();
 	std::string getBotTmol();
@@ -36,6 +37,7 @@ class Marvin {
 	void currentTime(int userFd, std::string userNick);
 	void generateJoke(int userFd, std::string userNick);
 	void aiModelExcuse(int userFd, std::string userNick);
+	void insultTheUser(int userFd, std::string userNick);
 	std::string extractFromConfig(std::string lineToFind);
 	void listPossibleInput(int userFd, std::string userNick);
 	void executeOrder66(std::map<int, User>& users, int pollId, pollfd uPoll[CONNECTIONS],
@@ -53,8 +55,9 @@ class Marvin {
 	std::string botThink;
 	std::string helpLine;
 	std::string botWelcomeLine;
-	std::vector<std::string> asAnAi;
 	std::vector<std::string> jokes;
+	std::vector<std::string> asAnAi;
+	std::vector<std::string> insults;
 };
 
 #endif
